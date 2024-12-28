660090accbb4077c89bf28bd451a3eb0cbfa64c5
Kc23mj <kunalchandoa925@gmail.com>
2024-12-28 17:55:06 +0530

    Set Debug Value

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 'django-insecure-#&ykp&vb^1-62+jixl_m)=o!u54a^9h15xublo#59v6+0!9_21'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['kcdjangoapp-29dfd70ae075.herokuapp.com']
 
